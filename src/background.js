3c3c90709cec1f2422bfd9ab0bc4a3c59929f61e
Kapil Easwar <bigpopakap@gmail.com>
2017-05-14 09:44:20 -0700

    More work

--- src/background.js
+++ src/background.js
@@ -1,11 +1,45 @@
-chrome.runtime.onMessage.addListener(function(message, sender, respond) {
-  debugger;
+function handleLoad(message, sender, respond) {
   if (message.canonicalUrl) {
-    // set the icon to a valid-looking one
+    if (message.canonicalUrl === sender.url) {
+      // set title to "On canonical already"
+      // disable action
+    } else {
+      // set title to "Clean url"
+      // enable action
+    }
   } else {
-    // set the icon to an invalid-looking one
+    // set title to "No canonical found"
+    // disable action
   }
 
   // show the page action
-  chrome.pageAction.show(sender.tab.id);
+  chrome.browserAction.show(sender.tab.id);
+}
+
+function handleDid(message, sender, respond) {
+  // set the title to "Done"
+  // set the icon to the one with a check mark
+  // disable action
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, respond) {
+  if (!message) return;
+
+  if (message.action === 'loadCanonical') {
+    handleLoad(message, sender, respond);
+  } else if (message.action === 'doneCanonical') {
+    handleDid(message, sender, respond);
+  }
+});
+
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    if (tabs && tabs.length > 0) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { action: 'canonicalize' },
+        function(response) {}
+      );
+    }
+  });
 });
--- src/inject.js
+++ src/inject.js
@@ -1,5 +1,21 @@
 (function() {
   var canonicalUrl = null;
+
+  chrome.runtime.onMessage.addListener(function(message, sender) {
+    debugger;
+    if (message && message.action === 'canonicalize') {
+      window.history.replaceState(
+        window.history.state,
+        'Canonicalized',
+        canonicalUrl
+      );
+
+      chrome.runtime.sendMessage({
+        action: 'doneCanonical'
+      });
+    }
+  });
+
   try {
     canonicalUrl = document.querySelector('link[rel="canonical"]').href;
   } catch (ex) {
@@ -7,6 +23,7 @@
   }
 
   chrome.runtime.sendMessage({
+    action: 'loadCanonical'
     canonicalUrl: canonicalUrl
   });
 })();
